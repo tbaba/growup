8fc79c2566a963ee1d6eec5226df1f4401277d66
Tatsuro BABA <harakirisoul@gmail.com>
2010-12-31 07:23:05 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Growup::Application.config.secret_token = 'b350db5988978526604b0c7195aa26f9a3ae7aed231d7261f7e6b5a6e042587c6d0a900402bba2ad8c53882bb0fb7bb031a32cca6c9a6223c8719c61d5067d98'
