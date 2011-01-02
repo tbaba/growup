df3ebac6de714eb3dfac8b9e0f6d718ab16c172e
Tatsuro BABA <harakirisoul@gmail.com>
2011-01-02 16:39:45 +0900

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110101151418) do
+ActiveRecord::Schema.define(:version => 20110102073816) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "title"
+    t.text     "detail"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
